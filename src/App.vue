0a27c8c9b0a42cc4a6804226f3b5569c763d1b58
Thiago099 <tecnologiasthiago@gmail.com>
2022-05-24 15:35:40 -0300

    [feature] base

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,76 @@
 <script setup lang="ts">
+import './colors.css'
+import './inputs.css'
+import './table.css'
+const colors = [
+  'black',
+  'red',
+  'orange',
+  'pink',
+  'green',
+  'cyan',
+  'blue',
+  'purple',
+];
 </script>
 
 <template>
   <header>
   </header>
   <main>
-    <div class="input-group pastel-input-red">
+    <div :class="`input-group pastel-${color}`" v-for="color in colors" :key="color">
       <input type="text" class="pastel-input" />
       <input type="submit" class="pastel-button">
-      <input type="submit" class="pastel-button">
     </div>
-      <!-- <input type="text" class="pastel-input pastel-input-green" />
-      <input type="text" class="pastel-input pastel-input-blue" />
-      <input type="text" class="pastel-input pastel-input-orange" />
-      <input type="text" class="pastel-input pastel-input-pink" />
-      <input type="text" class="pastel-input pastel-input-gray" />
-      <input type="text" class="pastel-input pastel-input-cyan" /> -->
+    <br>
+    <table :class="`pastel-${color}`" v-for="color in colors" :key="color">
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+      <tr>
+        <td>Ernst Handel</td>
+        <td>Roland Mendel</td>
+        <td>Austria</td>
+      </tr>
+      <tr>
+        <td>Island Trading</td>
+        <td>Helen Bennett</td>
+        <td>UK</td>
+      </tr>
+      <tr>
+        <td>Laughing Bacchus Winecellars</td>
+        <td>Yoshi Tannamuri</td>
+        <td>Canada</td>
+      </tr>
+      <tr>
+        <td>Magazzini Alimentari Riuniti</td>
+        <td>Giovanni Rovelli</td>
+        <td>Italy</td>
+      </tr>
+    </table>
   </main>
 </template>
 
 <style>
-#app{
-  /* center */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 90vh;
-}
+.input-group{
+    margin:5px;
+    display: inline-block;
 
-
-
-.pastel-input-default{
-  --background:rgb(255, 255, 255);
-  --border:#c2c2c2;
 }
-.pastel-input-red{
-  --background:rgb(255, 200, 200);
-  --border:rgb(255, 100, 100);
-}
-.pastel-input-green{
-  --background:rgb(200, 255, 200);
-  --border:rgb(100, 255, 100);
-}
-.pastel-input-blue{
-  --background:rgb(200, 200, 255);
-  --border:rgb(100, 100, 255);
-}
-.pastel-input-orange{
-  --background:rgb(255, 229, 200);
-  --border:rgb(255, 175, 100);
-}
-.pastel-input-pink{
-  --background:rgb(255, 200, 255);
-  --border:rgb(255, 100, 255);
-}
-.pastel-input-gray{
-  --background:rgb(200, 200, 200);
-  --border:rgb(100, 100, 100);
-}
-.pastel-input-cyan{
-  --background:rgb(200, 255, 255);
-  --border:rgb(100, 255, 255);
-}
-
-.pastel-button, .pastel-input{
-  padding: 5px;
-  padding-left: 0px;
-  padding-right: 0px;
-  border-radius: 3px;
-  background-color:var(--background) ;
-  border:2pt solid var(--border);
-  font-size: 13pt;
-  text-align : center;
-  outline-style: none;
-  
-}
-.pastel-button{
-  cursor: pointer;
-  padding-left: 10px;
-  padding-right: 10px;
-}
-.pastel-button:hover{
-  background-color:var(--border);
-  color:var(--background);
-}
-
-
-.input-group *:first-child{
-  border-right :none;
-  border-radius: 3px 0 0 3px;
-}
-
-.input-group *:last-child{
-  border-left :none;
-  border-radius: 0 3px 3px 0;
-}
-.input-group *:not(:last-child,:first-child){
-  border-left: none;
-  border-right: none;
-  border-radius: 0 0 0 0;
+table{
+    margin:5px;
+    display: inline-block;
 }
 </style>
