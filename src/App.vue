4260e11ff5e1ed73b7324d1408ae83bec74b268c
Thiago099 <tecnologiasthiago@gmail.com>
2022-05-25 10:36:25 -0300

    [feature] content

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import './colors.css'
-import './inputs.css'
-import './table.css'
-import './card.css'
-import './nav.css'
+import './css/colors.css'
+import './css/inputs.css'
+import './css/table.css'
+import './css/card.css'
+import './css/nav.css'
+import './css/content.css'
 const colors = [
   'black',
   'red',
@@ -20,82 +21,67 @@ const colors = [
   <header>
   </header>
   <main>
-    <div >
-      <div :class="`input-container pastel-${color}`" v-for="color in colors" :key="color"    >
-        <input type="text" class="pastel-input" />
-        <input type="submit" class="pastel-button">
-      </div>
-    </div>
-    <br>
-    <table :class="`pastel-table pastel-${color}`" v-for="color in colors" :key="color">
-      <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-      </tr>
-      <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Island Trading</td>
-        <td>Helen Bennett</td>
-        <td>UK</td>
-      </tr>
-      <tr>
-        <td>Laughing Bacchus Winecellars</td>
-        <td>Yoshi Tannamuri</td>
-        <td>Canada</td>
-      </tr>
-      <tr>
-        <td>Magazzini Alimentari Riuniti</td>
-        <td>Giovanni Rovelli</td>
-        <td>Italy</td>
-      </tr>
-    </table>
-    <div :class="`card pastel-${color}`" v-for="color in colors" :key="color">
+      <div class="row">
+        <div :class="`col-3 pastel-${color}`" v-for="color in colors" :key="color" >
+          <label for="a" class="input-label">Label:</label>
+          <div class="input-container">
+            <input type="text" class="pastel-input" />
+            <input type="submit" class="pastel-button">
+          </div>
+        </div>
+      <table :class="`col-3 pastel-table pastel-${color}`" v-for="color in colors" :key="color">
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+    <div :class="`col-3 card pastel-${color}`" v-for="color in colors" :key="color">
       <div class="card-header">Title</div>
       <div class="card-body">Content</div>
     </div>
-    <div :class="`nav pastel-${color}`" v-for="color in colors" :key="color">
+    <div :class="`col-3 nav pastel-${color}`" v-for="color in colors" :key="color">
       <div class="nav-item nav-selected">Home</div>
       <div class="nav-item">About</div>
       <div class="nav-item">Contact</div>
-
+    </div>
     </div>
   </main>
 </template>
 
 <style>
-.input-container{
-    margin:5px;
-    width: 437px;
-}
-.pastel-table{
-    margin:5px;
-    display: inline-block;
-}
-.card{
-  margin:5px;
-  width: 437px;
-  height: 200px;
-  display: inline-block;
-}
-.nav{
-  margin:5px;
-  width: 437px;
-  display: inline-block;
-}
+
+
+
 
 </style>
